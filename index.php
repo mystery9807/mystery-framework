1b79f05729c8eeb2af876d5e77ceed43c99c3556
mukhriddin-dev <khodievm@gmail.com>
2024-05-13 15:32:32 +0500

    created layout

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
-<?php
-require "./src/components/base/head/index.php";
-
+<?php require "./src/components/base/head/index.php" ?>
+<?php require "./src/components/base/header/index.php" ?>
+<main class="min-h-screen">
+    <?php require "./src/app.php" ?>
+</main>
+<?php require "./src/components/base/footer/index.php" ?>
